7d46999d70cf9a1d457f06c7288b16be23872c7b
Hagai Cohen <hagai.co@gmail.com>
2017-01-25 18:21:48 +0200

    test(utils): split huge test into chunks

--- test_utils.py
+++ test_utils.py
@@ -2,24 +2,6 @@
 import unittest
 import utils
 
-TESTED_SOURCES = [
-    ('[SUBBED] ABVideo', 'http://ww1.animeram.cc/one-piece/773/1'),
-    ('[SUBBED] ABVideo', 'http://ww1.animeram.cc/naruto-shippuden/1/1'),
-    ('[DUBBED] [HD] ABVideo', 'http://ww1.animeram.cc/naruto-shippuden/1/2'),
-    ('[SUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/4'),
-    ('[SUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/5'),
-    ('[DUBBED] yourupload', 'http://ww1.animeram.cc/naruto-shippuden/1/10'),
-    ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/11'),
-    ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/13'),
-    ('[SUBBED] videoweed', 'http://ww1.animeram.cc/naruto-shippuden/1/14'),
-    ('[SUBBED] yourupload', 'http://ww1.animeram.cc/naruto-shippuden/1/15'),
-    ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/16'),
-    ('[SUBBED] videonest', 'http://ww1.animeram.cc/naruto-shippuden/1/17'),
-    ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/18'),
-    ('[DUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/19'),
-    ('[DUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/20')
-]
-
 class MockedDialog(object):
     def __init__(self):
         pass
@@ -31,12 +13,51 @@ class MockedDialog(object):
         return False
 
 class TestUtils(unittest.TestCase):
-    def test_fetch_sources(self):
-        "fetch_sources fetches at least 50% of the sources"
-        dialog = MockedDialog()
-        fetched = utils.fetch_sources(TESTED_SOURCES, dialog, True)
+
+    def _test_sources(self, sources):
+        fetched = utils.fetch_sources(sources, MockedDialog(), True)
         # Make sure we have at least 50% hit rate
-        self.assertGreaterEqual(len(fetched), len(TESTED_SOURCES) / 2)
+        self.assertGreaterEqual(len(fetched), len(sources) / 2)
+        return fetched
+
+    def test_fetch_abvideo(self):
+        "fetch_sources fetches ABVideo"
+        self._test_sources([
+            ('[SUBBED] ABVideo', 'http://ww1.animeram.cc/one-piece/773/1'),
+            ('[SUBBED] ABVideo', 'http://ww1.animeram.cc/naruto-shippuden/1/1'),
+            ('[DUBBED] [HD] ABVideo', 'http://ww1.animeram.cc/naruto-shippuden/1/2'),
+        ])
+
+    def test_fetch_yourupload(self):
+        "fetch_sources fetches yourupload"
+        self._test_sources([
+            ('[DUBBED] yourupload', 'http://ww1.animeram.cc/naruto-shippuden/1/10'),
+            ('[SUBBED] yourupload', 'http://ww1.animeram.cc/naruto-shippuden/1/15'),
+        ])
+
+    def test_fetch_novamov(self):
+        "fetch_sources fetches novamov"
+        self._test_sources([
+            ('[SUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/5'),
+            ('[DUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/19'),
+            ('[DUBBED] novamov', 'http://ww1.animeram.cc/naruto-shippuden/1/20')
+        ])
+
+    def test_fetch_mp4upload(self):
+        "fetch_sources fetches novamov"
+        self._test_sources([
+            ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/11'),
+            ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/13'),
+            ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/16'),
+            ('[SUBBED] mp4upload', 'http://ww1.animeram.cc/naruto-shippuden/1/18'),
+        ])
+
+    def test_fetch_others(self):
+        "fetch_sources fetches others less common providers"
+        self._test_sources([
+            ('[SUBBED] videoweed', 'http://ww1.animeram.cc/naruto-shippuden/1/14'),
+            ('[SUBBED] videonest', 'http://ww1.animeram.cc/naruto-shippuden/1/17'),
+        ])
 
 if __name__ == "__main__":
     unittest.main()
