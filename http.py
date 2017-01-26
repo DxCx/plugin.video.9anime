68f6593754278314a4b31aab32c17d424bd51b78
Hagai Cohen <hagai.co@gmail.com>
2017-01-26 15:04:56 +0200

    replace http implementation

--- http.py
+++ http.py
@@ -1,87 +1,65 @@
 import urllib2
 from urllib2 import URLError
-import ssl
 import httplib
 import socket
 
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+import ssl
+
 _USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
+_SESSION = None
 
-class HeadRequest(urllib2.Request):
-    def get_method(self):
-        return "HEAD"
+class SSLAdapter(HTTPAdapter):
+    '''An HTTPS Transport Adapter that uses an arbitrary SSL version.'''
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections,
+                              maxsize=maxsize,
+                              block=block)
 
-class HTTPRedirectHandler(urllib2.HTTPRedirectHandler):
+class PrepReq(object):
+    def __init__(self):
+        self._dict = {}
 
-    def http_error_302(self, req, fp, code, msg, headers):
-        for no302 in noredirects:
-            if no302.endswith("/") and no302 == req.get_full_url():
-                if no302==req.get_full_url():
-                    #return self.parent._open(req)
-                    return (req, fp, 200, msg, headers)
-            elif no302 in req.get_full_url():
-                #return self.parent._open(req)
-                return (req, fp, 200, msg, headers)
-        else:
-            result=urllib2.HTTPRedirectHandler.http_error_302(self,req, fp, code, msg, headers)
-            result.status=code
-            return result
+    def add_headers(self, key, value):
+        self._dict[key] = value
 
-    def http_error_301(self, *args,**kwargs):
-        self.http_error_302(*args,**kwargs)
+    @property
+    def headers(self):
+        return self._dict
 
-class HTTPErrorProcessor(urllib2.HTTPErrorProcessor):
-    """Process HTTP error responses."""
-    handler_order = 1000  # after all other processing
+def Session():
+    global _SESSION
+    if not _SESSION:
+        s = requests.Session()
+        s.mount('https://', SSLAdapter())
+        s.headers.update({
+            'User-Agent': _USER_AGENT,
+        })
+        _SESSION = s
 
-    def http_response(self, request, response):
-        if response.code==302:
-            for no302 in noredirects:
-                if no302.endswith("/") and no302 == request.get_full_url():
-                    if no302==request.get_full_url():
-                        return response
-                elif no302 in request.get_full_url():
-                    return response
-        return urllib2.HTTPErrorProcessor.http_response(self, request, response)
+    return _SESSION
 
-class NoCertSSLCon(httplib.HTTPConnection):
-    default_port = httplib.HTTPS_PORT
-    def __init__(self, *args, **kwargs):
-        httplib.HTTPConnection.__init__(self, *args, **kwargs)
-
-    def connect(self):
-        #google app engine does not have source adress. r u drunk??
-        if hasattr(self,"source_address"):
-            sock = socket.create_connection((self.host, self.port), self.timeout, self.source_address)
-        else:
-            sock = socket.create_connection((self.host, self.port), self.timeout)
-
-        if self._tunnel_host:
-            self.sock = sock
-            self._tunnel()
-        self.sock = ssl.wrap_socket(sock,cert_reqs=ssl.CERT_NONE)
-
-class HTTPSHandler(urllib2.HTTPSHandler):
-    def __init__(self, *args, **kwargs):
-        urllib2.HTTPSHandler.__init__(self, *args, **kwargs)
-
-    def https_open(self, req):
-        return self.do_open(NoCertSSLCon,req)
-
-def _process_request(req, set_request=None):
-    opener = urllib2.build_opener(HTTPSHandler)
-    req.add_header('User-Agent', _USER_AGENT)
+def send_request(url, data=None, set_request=None, head=False):
+    session = Session()
+    r = PrepReq()
     if set_request:
-        req = set_request(req)
+        r = set_request(r)
 
-    return opener.open(req)
+    kargs = {
+        'headers': r.headers,
+        'verify': False,
+        'url': url,
+    }
 
-def send_request(url, data=None, set_request=None):
+    if head:
+        return session.head(**kargs)
+
     if data:
         data = urllib.urlencode(data)
-        req = urllib2.Request(url, data)
-    else:
-        req = urllib2.Request(url)
-    return _process_request(req, set_request)
+        return session.post(data=data, **kargs)
+    return session.get(**kargs)
 
 def head_request(url, set_request=None):
-    return _process_request(HeadRequest(url), set_request)
+    return send_request(url, set_request=set_request, head=True)
