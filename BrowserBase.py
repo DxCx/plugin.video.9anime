0bb2c15e0c920738775c55d18e37bfb8d4f7073b
Hagai Cohen <hagai.co@gmail.com>
2017-05-15 19:06:47 +0300

    chore(browser-base): add set-request argument

--- BrowserBase.py
+++ BrowserBase.py
@@ -11,13 +11,13 @@ class BrowserBase(object):
             url = url[1:]
         return "%s/%s" % (self._BASE_URL, url)
 
-    def _send_request(self, url, data=None):
-        return http.send_request(url, data).text
+    def _send_request(self, url, data=None, set_request=None):
+        return http.send_request(url, data, set_request).text
 
-    def _post_request(self, url, data={}):
-        return self._send_request(url, data)
+    def _post_request(self, url, data={}, set_request=None):
+        return self._send_request(url, data, set_request)
 
-    def _get_request(self, url, data=None):
+    def _get_request(self, url, data=None, set_request=None):
         if data:
             url = "%s?%s" % (url, urllib.urlencode(data))
-        return self._send_request(url)
+        return self._send_request(url, None, set_request)
