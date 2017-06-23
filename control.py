03daf19cd3da2b9d4435b8181c2bb6fc537d7f7c
Hagai Cohen <hagai.co@gmail.com>
2017-06-23 23:56:46 +0300

    chore(control): add setContent helper

--- control.py
+++ control.py
@@ -17,6 +17,9 @@ __settings__ = xbmcaddon.Addon(ADDON_NAME)
 __language__ = __settings__.getLocalizedString
 CACHE = StorageServer.StorageServer("%s.animeinfo" % ADDON_NAME, 24)
 
+def setContent(contentType):
+    xbmcplugin.setContent(HANDLE, contentType)
+
 def getSetting(key):
     return __settings__.getSetting(key)
 
