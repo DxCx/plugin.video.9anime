91403285cd5d7aeff31165ca215e9ebfe62eb85c
Hagai Cohen <hagai.co@gmail.com>
2017-11-26 15:42:15 +0200

    fix: update ts mechanizem

--- NineAnimeUrlExtender.py
+++ NineAnimeUrlExtender.py
@@ -1,20 +1,75 @@
 import re
-
+from string import ascii_lowercase as lc, ascii_uppercase as uc, maketrans
 
 class NineAnimeUrlExtender:
-    _ts_value_regex = re.compile(ur"<body.*data-ts\s*=['\"](\d+)['\"]")
+    _TS_MAP_TABLE = [i for i in uc if ord(i) % 2 != 0] + [i for i in uc if ord(i) % 2 == 0]
+    _ts_value_regex = re.compile(ur"<body.*data-ts\s*=[\"]([^\"]+?)[\"]")
+    _server_value_regex = \
+    re.compile(ur"<div\sclass=\"server\srow\"\s.+?data-id=\"(\d+)\".*?>(.+?)\<\/div\>")
+    _active_ep_regex = re.compile(ur"\<a\sclass=\"active\".+?")
 
     def __init__(self):
         pass
 
     @classmethod
-    def get_ts_value(cls, content):
-        return cls._ts_value_regex.findall(content)[0]
+    def rot_dict(cls, obj):
+        newObj = {}
+
+        for key, value in obj.iteritems():
+            if type(value) is unicode or type(value) is str:
+                if value.startswith('.'):
+                    newObj[key] = cls.rot_string(value[1:])
+                else:
+                    newObj[key] = value
+            elif type(value) is dict:
+                newObj[key] = cls.rot_dict(value)
+            else:
+                newObj[key] = value
+
+        return newObj
 
     @classmethod
-    def get_extra_url_parameter(cls, id, update, ts):
+    def rot_string(cls, content):
+        RotBy = 8
+        lookup = maketrans(lc + uc, lc[RotBy:] + lc[:RotBy] + uc[RotBy:] + uc[:RotBy])
+        decoded = str(content).translate(lookup)
+        return decoded
+
+    @classmethod
+    def get_server_value(cls, content):
+        server_values = cls._server_value_regex.findall(content)
+        for (server_id, episodes) in server_values:
+            if len(cls._active_ep_regex.findall(episodes)):
+                return int(server_id, 10)
+
+        raise Exception("Cant extract server id")
+
+    @classmethod
+    def get_ts_value(cls, content):
+        ts_value = cls._ts_value_regex.findall(content)[0]
+        return cls._decode_ts_value(ts_value)
+
+    @classmethod
+    def _decode_ts_value(cls, ts):
+        decoded = ""
+        for c in ts:
+            replaced = False
+            if c not in cls._TS_MAP_TABLE:
+                decoded += c
+                continue
+            decoded += uc[cls._TS_MAP_TABLE.index(c)]
+
+        return decoded.decode("base64")
+
+    @classmethod
+    def get_extra_url_parameter(cls, id, update, server, ts):
         DD = 'gIXCaNh'
-        params = [('id', str(id)), ('update', str(update)), ('ts', str(ts))]
+        params = [
+            ('id', str(id)),
+            ('update', str(update)),
+            ('ts', str(ts)),
+            ('server', str(server)),
+        ]
 
         o = cls._s(DD)
         for i in params:
