0d4a1cc80f3a6afd8ce83cf49764461a81ff9705
Hagai Cohen <hagai.co@gmail.com>
2018-02-18 10:25:51 +0200

    Revert "chore: fix players (MyCloud/RapidVideo)"
    This reverts commit 3b2b9163c43595fd8ed46471e6533c19762a3525.

--- resources/lib/ui/http.py
+++ resources/lib/ui/http.py
@@ -48,12 +48,6 @@ def Session():
 def raw_url(url):
     return _strip_url(url)[0]
 
-def get_referer(url):
-    url, headers = _strip_url(url)
-    if _REFERER_HEADER in headers:
-        return headers[_REFERER_HEADER]
-    return None
-
 def send_request(url, data=None, set_request=None, head=False):
     session = Session()
     target_url, headers = _strip_url(url)
