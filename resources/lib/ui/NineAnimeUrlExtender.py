38ba1fbf3a9c2edd8db760f451b9f0ba9677a9f2
Hagai Cohen <hagai.co@gmail.com>
2018-06-19 00:17:30 +0300

    fix: NineAnimeExtender Token (By ThePyrotechnic)

--- resources/lib/ui/NineAnimeUrlExtender.py
+++ resources/lib/ui/NineAnimeUrlExtender.py
@@ -85,7 +85,7 @@ class NineAnimeUrlExtender:
 
     @classmethod
     def get_extra_url_parameter(cls, id, server, ts):
-        DD = 'bVZX0bdD'
+        DD = '8s5IB3Gt'
         params = [
             ('id', str(id)),
             ('ts', str(ts)),
